17bf97381b39cafc202b20e1e505087168cac366
Marco <marcosartorelli24@gmail.com>
2018-06-21 12:19:39 +0200

    smth

--- src/test/BinTreeTest.java
+++ src/test/BinTreeTest.java
@@ -1,12 +1,14 @@
 package test;
 
+import support.InputDati;
 import tree.BinaryTree;
 
 public class BinTreeTest {
 	public static void main(String[] args){
-		BinaryTree<Integer> root = new BinaryTree<>(5);
-		for(int i=0;i<10;i++){
-			root.addChild(i);
+		BinaryTree<Integer> root = new BinaryTree<>(7);
+		for(int i=0;i<6;i++) {
+			int t = InputDati.leggiIntero("Inserisci elemento albero : ");
+			root.addChild(t);
 		}
 		root.print();
 	}
--- src/tree/BinaryTree.java
+++ src/tree/BinaryTree.java
@@ -1,13 +1,27 @@
 package tree;
 
-public class BinaryTree<T extends Comparable> {
-	BinaryTree<T> leftChild,rightChild;
-	T el;
+public class BinaryTree<T extends Comparable<? super T>> { //perche super?
+	private BinaryTree<T> leftChild,rightChild;
+	private T el;
+	private final int depth;
 
 	public BinaryTree(T el) {
+		this(el,0);
+	}
+
+	public BinaryTree(T el,int depth){
+		this.depth = depth;
 		this.el = el;
 	}
 
+	/**
+	 * Il metodo riceve un elemento T, controlla se e` maggiore o minore del padre,
+	 * se non ha ancora un figlio (destro o sinistro a seconda che sia maggiore o minore)
+	 * modifica il riferimento child da null all'oggetto appena creato contenente il parametro,
+	 * nel caso invece il riferimento del figlio sia diverso da null delega il compito al figlio
+	 *
+	 * @param child elemento da aggiungere all'albero
+	 */
 	public void addChild(T child){
 		if(child.compareTo(el)<0){
 			if(leftChild!=null) leftChild.addChild(child);
@@ -20,13 +34,13 @@ public class BinaryTree<T extends Comparable> {
 	}
 
 	public void print(){
-		System.out.println("Elemento: "+el);
+		System.out.println("Elemento: "+el+" profondita` : "+count);
+		count++;
+
 		if(leftChild!=null) {
-			System.out.println("Elementi ramo sinistro: ");
 			leftChild.print();
 		}
 		if(rightChild!=null) {
-			System.out.println("Elementi ramo dx: ");
 			rightChild.print();
 		}
 	}
